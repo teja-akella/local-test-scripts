6aca53849e1740302f573ea5895cb13535a94823
Yash Dua <yashdua1@gmail.com>
2025-03-06 21:25:27 -0800

    File Ingestion test now sends slack message on failure

--- test_file_ingestion.py
+++ test_file_ingestion.py
@@ -36,5 +36,8 @@ def test_file_upload(get_bearer_token, project_id="e4cc55c9-305d-478f-b285-96d43
 )
 def test_delete_file(get_bearer_token, file_dataset_id="", project_id="e4cc55c9-305d-478f-b285-96d436229fba"):
 	file_dataset_id = get_file_dataset_id(get_bearer_token, filename="Public Beta Launch Test Cases - requirements import structure.pdf", project_id="e4cc55c9-305d-478f-b285-96d436229fba")
+    if file_dataset_id == None:
+        send_slack_message("Unable to get file dataset id for uploaded file. File likely did not finish uploading. Please check latest Jenkins run at https://dev.jenkins.101gen.ai/job/File%20Ingestion%20Test")
     # for jenkins: show line chart of ingestion time across runs
+
 	delete_file(get_bearer_token, file_dataset_id, project_id="e4cc55c9-305d-478f-b285-96d436229fba")
